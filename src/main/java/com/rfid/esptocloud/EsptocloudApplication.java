f120e3a61140c695edca73aa7cc0a143fcee3c82
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-05 20:59:58 +0200

    initial commit for project structure

---
+++ src/main/java/com/rfid/esptocloud/EsptocloudApplication.java
@@ -0,0 +1,13 @@
+package com.rfid.esptocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsptocloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsptocloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rfid/esptocloud/EsptocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfid.esptocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsptocloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
