5bf175bd44beb4a004589b51bc245e4e2aadf0e6
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-06 20:44:15 +0200

    Card class release of basic structure

---
+++ src/main/java/com/rfid/esptocloud/domain/Card.java
@@ -0,0 +1,13 @@
+package com.rfid.esptocloud.domain;
+
+public class Card {
+
+
+    public Long Id;
+    public String cardOwnerName;
+    public Float initialBalance;
+    public Float newBalance;
+    public Integer transportFare;
+
+
+}
