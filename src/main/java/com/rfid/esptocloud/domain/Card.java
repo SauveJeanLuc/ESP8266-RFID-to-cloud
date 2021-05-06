c0c41b8ebd72c5221cb6c93e3d47a84e2b48b0ba
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-06 20:49:19 +0200

    Ended with Card.java

--- src/main/java/com/rfid/esptocloud/domain/Card.java
+++ src/main/java/com/rfid/esptocloud/domain/Card.java
@@ -1,13 +1,31 @@
 package com.rfid.esptocloud.domain;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "card")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Card {
 
-
+    @Id
+    @Column("card_id")
     public Long Id;
+    @Column("card_owner_name")
     public String cardOwnerName;
+    @Column("initial_balance")
     public Float initialBalance;
+    @Column("new_balance")
     public Float newBalance;
+    @Column("transport_fare")
     public Integer transportFare;
-
 
 }
